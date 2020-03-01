84589b5a9b0f2fc94673c1d8ac8786b8fba3303b
league <league@imac-31.attlocal.net>
2020-03-01 11:00:58 -0800

    swapping digits, sorted array checker, sorting algorithms(IP)

--- src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
+++ src/_00_Sorting_Algorithms/_00_SortedArrayChecker.java
@@ -6,14 +6,39 @@ public class _00_SortedArrayChecker {
 	//   and it returns a boolean.
 	//   The method returns true if the integer
 	//   array is in ascending order and false otherwise
-
+static boolean intArraySorted(int[] input) {
+	boolean inOrder = false;
+	for(int i = 0; i<input.length-1;i++) {
+		System.out.println(input[i] + " " + input[i+1]);
+		if(input[i]<=input[i+1]) {
+			inOrder = true;
+		}
+		else {
+			
+			return false;
+		}
+	}
+	return inOrder;
+}
 	
 	//2. Write a static method called doubleArraySorted. 
 	//   This method takes in an array of doubles
 	//   and it returns a boolean.
 	//   The method returns true if the double
 	//   array is in ascending order and false otherwise
-
+static boolean doubleArraySorted(double[] input) {
+	boolean inOrder = false;
+	for(int i = 0; i<input.length-1;i++) {
+		if(input[i]<=input[i+1]) {
+			inOrder = true;
+		}
+		else {
+			return false;
+		}
+	}
+	
+	return inOrder;
+}
 	
 	//3. Write a static method called charArraySorted. 
 	//   This method takes in an array of characters
@@ -21,7 +46,18 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the character
 	//   array is in alphabetical order and false otherwise
 	//   (You can compare characters just like integers)
-
+static boolean charArraySorted(char[] input) {
+	boolean inOrder = false;
+	for(int i = 0; i<input.length-1;i++) {
+		if(input[i]<=input[i+1]) {
+			inOrder = true;
+		}
+		else {
+			return false;
+		}
+	}
+	return inOrder;
+}
 	
 	//4. Write a static method called stringArraySorted. 
 	//   This method takes in an array of Strings
@@ -29,5 +65,16 @@ public class _00_SortedArrayChecker {
 	//   The method returns true if the String
 	//   array is in alphabetical order and false otherwise
 	//   (Use the compareTo(String) method)
-
+static boolean stringArraySorted (String[] input) {
+	boolean inOrder = false;
+	for(int i = 0; i<input.length-1;i++) {
+		if(input[i].compareTo(input[i+1])<0) {
+			inOrder = true;
+		}
+		else {
+			return false;
+		}
+	}
+	return inOrder;
+}
 }
