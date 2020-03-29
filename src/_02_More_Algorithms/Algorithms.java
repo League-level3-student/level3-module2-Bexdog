3db3459951a226340c88623f3d38a9844e0c8bc7
beckh <beckh@McKid>
2020-03-29 11:10:48 -0700

    algorithms, searching and sorting algorithms, more and more tests

--- src/_02_More_Algorithms/Algorithms.java
+++ src/_02_More_Algorithms/Algorithms.java
@@ -39,4 +39,87 @@ public class Algorithms {
 			}
 			return words.get(longest);
 		}
+		public static boolean containsSOS(List<String> message) {
+			for(int i = 0;i< message.size(); i++) {
+				if(message.get(i).equals(" ... --- ... ")) {					
+					return true;
+				}
+			}	
+			return false;
+		}
+		public static List<Double> sortScores(List<Double> results) {
+			for(int j = 0;  j<results.size();j++) {
+			for(int i = 0; i<results.size();i++) {
+				if(results.get(i)>results.get(j)) {
+					Double x = results.get(i);
+					results.set(i, results.get(j)) ;
+					results.set(j, x);
+				}
+			}
+			}
+			return results;
+		}
+		public static List<String> sortDNA(List<String> dna) {
+			for(int j =0;j<dna.size();j++) {
+				for(int i =0;i<dna.size();i++) {
+					if(dna.get(i).length()>dna.get(j).length()) {
+						String x = dna.get(i);
+						dna.set(i, dna.get(j));
+						dna.set(j, x);
+						
+					}
+				}
+			}
+			
+			return dna;
+		}
+		public static List<String> sortWords(List<String> words){
+			for(int j = 0; j<words.size();j++) {
+				for(int i = 0; i<words.size();i++) {
+					if(words.get(i).compareTo(words.get(j))>0) {//first is greater
+						String x = words.get(i);
+						words.set(i, words.get(j));
+						words.set(j, x);					
+						}
+					
+				}
+			}
+			
+			return words;
+		}
+		public static String multiply(int factor1, int factor2) {
+			int j = factor1*factor2;
+			String end = factor1+" x "+factor2+" = "+j;
+			return end;
+		}
+		public static boolean isPrime(int number) {
+			for(int i = 2;i<=number/2;i++) {
+				if(number%i==0) {
+					return false;
+				}
+			}
+			return true;
+		}
+		public static boolean isSquare(int number) {
+			if(number == 1) {
+				return true;
+			}
+			for(int i = 1; i<=number/2;i++) {
+				if(i*i == number) {
+					return true;
+				}
+			}
+			return false;
+		}
+		public static boolean isCube(int number) {
+			if(number == 1) {
+				return true;
+			}
+			for(int i = 1; i<=number/3;i++) {
+				if(i*i*i == number) {
+					return true;
+				}
+			}
+			return false;
+		}
 }
